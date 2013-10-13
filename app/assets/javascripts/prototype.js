502b9dc08a211b25fd42b37b995b2b39ac87450c
Cheryl Wu <grungerabbit@gmail.com>
2013-10-13 12:02:41 -0400

    fixed cards

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -1,49 +1,31 @@
 (function($){
 	var eatWithMe = {
 		'config' : {
-			
-			'test' : "#facade",
-			
 			'charSelect' : ".screen--character",
 			'status' : ".foods .status"
 		},
 
 		'init' : function() {
-			
-			
 			$("body").on("click", ".screen--character .character", function() {
-				
-				
 				var chars = eatWithMe.characterSelect($(this));
 				chars.about();
-				
-				
 			})
 			
 			$(".information").hide();
-
-		
 		},
 
 		// CHECK YOUR COMMAS
 		'characterSelect' : function(element) {
-			
 			function selectBg() {
-				//eatWithMe.config.charSelect.find(".character").each( function() {
-			
-				
 				element.addClass("selected");
-				element.siblings().removeClass("selected");	
-				element.siblings().find(".information").hide();
+				element.siblings(".character").removeClass("selected");	
+				element.siblings(".character").find(".information").hide();
 				info();
-				
 			}
 			function info() {
 				var height = parseInt(element.height(), 10);
-				var width = parseInt(element.width(), 10);
+				var width = parseInt(element.width() - 2, 10);
 				
-				console.log(height)
-			
 				element.find(".information").fadeIn().css("margin-top", -height).css("margin-left", -15).height(height-32).width(width);
 			}
 	
@@ -51,11 +33,9 @@
 			return {
 				'about' : function() {
 					selectBg();
-					//other();
 				}
 			}
 		}
-
 	};
 
 	$(document).ready( function() {
